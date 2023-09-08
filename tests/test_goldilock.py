7f944ba9c931be3c30415c0171b61fdaf22b01db
NandhiniKRC <nandhini.krc@gmail.com>
2023-09-08 10:07:19 +0530

    Initial commit

---
+++ tests/test_goldilock.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `goldilock` package."""
+
+
+import unittest
+
+from goldilock import goldilock
+
+
+class TestGoldilock(unittest.TestCase):
+    """Tests for `goldilock` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
